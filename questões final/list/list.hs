25c57d30b4263b208235218771d248d9c184cb6f
RobertoCoutinho <robertoo.coutinho2014@gmail.com>
2020-10-14 04:30:15 -0300

    listas infinitas

---
+++ questões final/list/list.hs
@@ -0,0 +1,11 @@
+-- collatz :: Integer -> [Integer]
+-- collatz 1 = [1]
+-- collatz x = [x] ++ collatz (seguinte x)
+
+-- seguinte :: Integer -> Integer
+-- seguinte 1 = 1
+-- seguinte n = 
+--     if n `mod`  2 == 0
+--         then n `div` 2
+--     else
+--         n * 3 + 1
